6278956b8a47ed50ab92879353f6f38a7a4be6bd
jeremy chow <xelloxzxd@gmail.com>
2023-10-31 23:56:36 +0800

    update: add CORS

--- router.go
+++ router.go
@@ -2,12 +2,13 @@ package main
 
 import (
 	"context"
-	"os"
 	"fmt"
+	"os"
 
 	"github.com/cs301-2023-g3t3/points-ledger/controllers"
-	"github.com/cs301-2023-g3t3/points-ledger/models"
 	"github.com/cs301-2023-g3t3/points-ledger/middlewares"
+	"github.com/cs301-2023-g3t3/points-ledger/models"
+	"github.com/gin-contrib/cors"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
@@ -37,6 +38,7 @@ func InitRoutes() {
 	router := gin.New()
 	// router.Use(gin.Logger())
 	router.Use(gin.Recovery())
+	router.Use(cors.Default())
 	router.Use(middlewares.LoggingMiddleware())
 
 	health := new(controllers.HealthController)

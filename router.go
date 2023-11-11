f159e61f01b6f93174ef210a92faf469b28162ef
Yao Long <yaolongt@gmail.com>
2023-11-11 11:08:02 +0800

    add: allow headers

--- router.go
+++ router.go
@@ -40,6 +40,13 @@ func InitRoutes() {
 	router.Use(gin.Recovery())
 	router.Use(cors.Default())
 	router.Use(middlewares.LoggingMiddleware())
+  
+	config := cors.DefaultConfig()
+	config.AllowHeaders = append(config.AllowHeaders, "Authorization")
+	config.AllowHeaders = append(config.AllowHeaders, "id_token")
+	config.AllowAllOrigins = true
+
+	router.Use(cors.New(config))
 
 	health := new(controllers.HealthController)
 	points := controllers.NewPointsController(*models.DB)
